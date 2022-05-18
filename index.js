5980b35703c49e6e428c10dcd68afc8b413e0e6f
burakceylanz <burakbayarceylan@gmail.com>
2022-05-18 03:35:33 +0300

    Cors Updated

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
-app.use(cors({ credentials: true, origin: 'localhost' }))
+//app.use(cors({ credentials: false, origin: 'http://localhost:3000/' }))
+
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 app.use("/memories", memoryRouter)
 
